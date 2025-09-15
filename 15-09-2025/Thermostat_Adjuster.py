b50d38c49a7e6c5421a3f7fc631e473cf89e4841
Karthik Jayadevan <karthikjayd@gmail.com>
2025-09-15 16:51:35 +0200

    reformatted with ruff

--- 15-09-2025/Thermostat_Adjuster.py
+++ 15-09-2025/Thermostat_Adjuster.py
@@ -8,18 +8,20 @@ Given the current temperature of a room and a target temperature, return a strin
     Return "hold" if the current temperature is equal to the target.
 """
 
+
 def adjust_thermostat(temp: float, target: float) -> str:
     is_cool = temp < target
     is_warm = temp > target
 
     if is_cool:
-        return 'heat'
+        return "heat"
     elif is_warm:
-        return 'cool'
+        return "cool"
     else:
-        return 'hold'
+        return "hold"
 
-if __name__=='__main__':
+
+if __name__ == "__main__":
     current_temp: float = 23.5
     target_temp: float = 27.1
     print(adjust_thermostat(current_temp, target_temp))
