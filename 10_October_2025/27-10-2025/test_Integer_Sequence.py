4fadd24a2006c1fab736285374df5d21756e7d60
Karthik <qc@karthikjayd.com>
2025-10-27 15:59:25 +0100

    complete October 27

---
+++ 10_October_2025/27-10-2025/test_Integer_Sequence.py
@@ -0,0 +1,8 @@
+from Integer_Sequence import sequence
+
+
+def test_sequence() -> None:
+    assert sequence(5) == "12345"
+    assert sequence(10) == "12345678910"
+    assert sequence(1) == "1"
+    assert sequence(27) == "123456789101112131415161718192021222324252627"
